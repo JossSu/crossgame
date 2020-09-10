3683cea98b2f88c23314e9f39a475a78e88ddff3
Joss Su <sujiaxu0202@gmail.com>
2020-09-10 15:08:54 +0800

    Updates

---
+++ precache-manifest.56b5d3733d7e83e46d6f4d6a92ef8c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7a2b650f04dc569164e3352179bb82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b49c200485e31ca87b65",
+    "url": "/static/css/main.f4b9b883.chunk.css"
+  },
+  {
+    "revision": "02678df93174470d3aa8",
+    "url": "/static/js/2.68fa4af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68fa4af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49c200485e31ca87b65",
+    "url": "/static/js/main.354f78c7.chunk.js"
+  },
+  {
+    "revision": "d41b34be0f7e0e7ee9d7",
+    "url": "/static/js/runtime-main.48bcae34.js"
+  }
+]);
