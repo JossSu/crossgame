c431166761516b6e7bed017ec4f3a148c368a2bb
Joss Su <sujiaxu0202@gmail.com>
2020-09-10 15:18:02 +0800

    Updates

---
+++ precache-manifest.39bfbb6cc86951763f9818c77c6052d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37362ca4d1201663fd533393ab1af796",
+    "url": "/crossgame/index.html"
+  },
+  {
+    "revision": "b49c200485e31ca87b65",
+    "url": "/crossgame/static/css/main.f4b9b883.chunk.css"
+  },
+  {
+    "revision": "02678df93174470d3aa8",
+    "url": "/crossgame/static/js/2.68fa4af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crossgame/static/js/2.68fa4af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49c200485e31ca87b65",
+    "url": "/crossgame/static/js/main.354f78c7.chunk.js"
+  },
+  {
+    "revision": "d1c28aceb6e085f4ea17",
+    "url": "/crossgame/static/js/runtime-main.8189a61f.js"
+  }
+]);
